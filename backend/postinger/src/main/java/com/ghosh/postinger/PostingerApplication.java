450025385f6406cb8a102ab4af10e59446e4b497
JITGH <gjit515@gmail.com>
2024-12-15 14:37:16 +0530

    chore:setup frontend & backend vite & springboot

---
+++ backend/postinger/src/main/java/com/ghosh/postinger/PostingerApplication.java
@@ -0,0 +1,13 @@
+package com.ghosh.postinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostingerApplication.class, args);
+	}
+
+}
---
+++ backend/postinger/src/test/java/com/ghosh/postinger/PostingerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghosh.postinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostingerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
